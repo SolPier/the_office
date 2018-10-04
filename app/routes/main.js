826828ac4d4c1a3ab5676ce1ce4ec64ccd6b3c42
Aurelien 'Sol' Pieropan <sol.pieropan@gmail.com>
2018-10-04 23:13:11 +0200

    s1 data ok

--- app/routes/main.js
+++ app/routes/main.js
@@ -14,21 +14,21 @@ export default Route.extend({
           { number: 6, name: 'Hot Girl',      videoUrl: 'https://openload.co/embed/_bBcX35_23A/S01E06.mkv.mp4' }
         ]
       },
-      {
-        number: 2,
-        episodes: [
-          { number: 1, name: 'The Dundies',        videoUrl: 'https://openload.co/embed/qAdOk03kJNI/S02E01.mkv.mp4' },
-          { number: 2, name: 'Sexual Harassment ', videoUrl: 'https://openload.co/embed/ACbw_0mObzo/S02E02.mkv.mp4' },
-          { number: 3, name: 'Office Olympics ',   videoUrl: 'https://openload.co/embed/wA_jgjGOejA/S02E03.mkv.mp4' }
-        ]
-      },
-      {
-        number: 3,
-        episodes: [
-          { number: 1, name: 'Gay Witch Hunt ', videoUrl: 'https://openload.co/embed/BFvO5w7oSG8/S03E01.mkv.mp4' },
-          { number: 2, name: 'The Convention ', videoUrl: 'https://openload.co/embed/KDRq_8Pcsl4/S03E02.mkv.mp4' }
-        ]
-      }
+      // {
+      //   number: 2,
+      //   episodes: [
+      //     { number: 1, name: 'The Dundies',        videoUrl: 'https://openload.co/embed/qAdOk03kJNI/S02E01.mkv.mp4' },
+      //     { number: 2, name: 'Sexual Harassment ', videoUrl: 'https://openload.co/embed/ACbw_0mObzo/S02E02.mkv.mp4' },
+      //     { number: 3, name: 'Office Olympics ',   videoUrl: 'https://openload.co/embed/wA_jgjGOejA/S02E03.mkv.mp4' }
+      //   ]
+      // },
+      // {
+      //   number: 3,
+      //   episodes: [
+      //     { number: 1, name: 'Gay Witch Hunt ', videoUrl: 'https://openload.co/embed/BFvO5w7oSG8/S03E01.mkv.mp4' },
+      //     { number: 2, name: 'The Convention ', videoUrl: 'https://openload.co/embed/KDRq_8Pcsl4/S03E02.mkv.mp4' }
+      //   ]
+      // }
     ]
   }
 });
