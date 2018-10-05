83b30cc821e2769fcb2483937cee2730758ba3e7
Aurelien 'Sol' Pieropan <sol.pieropan@gmail.com>
2018-10-06 00:35:48 +0200

    added copy-box component

--- app/components/episode.js
+++ app/components/episode.js
@@ -3,45 +3,8 @@ import { later } from '@ember/runloop'
 
 export default class Episode extends Component {
   @tracked episodeIsVisible = false
-  @tracked instructionsAreVisible = false
-  @tracked hasCopiedLink = false
-  subtitleUId = `subtitles-s${this.args.seasonNumber}e${this.args.episode.number}`
-  currentUrl = window.location.href
 
   toggleEpisodeVisibility() {
     this.episodeIsVisible = !this.episodeIsVisible
-    this.instructionsAreVisible = false
-  }
-
-  toggleInstructions() {
-    this.instructionsAreVisible = !this.instructionsAreVisible
-  }
-
-  deselectAll() {
-    var element = document.activeElement;
-
-    if (element && /INPUT|TEXTAREA/i.test(element.tagName)) {
-      if ('selectionStart' in element) {
-        element.selectionEnd = element.selectionStart;
-      }
-      element.blur();
-    }
-
-    if (window.getSelection) { // All browsers, except IE <=8
-      window.getSelection().removeAllRanges();
-    } else if (document.selection) { // IE <=8
-      document.selection.empty();
-    }
-  }
-
-  copySubtitleLinkToClipboard() {
-    document.getElementById(this.subtitleUId).select()
-    document.execCommand("copy")
-    this.deselectAll()
-
-    this.hasCopiedLink = true
-    later(this, function() {
-      this.hasCopiedLink = false
-    }, 1000)
   }
 }
--- app/components/instructions.js
+++ app/components/instructions.js
@@ -1,4 +1,9 @@
-import Component from "sparkles-component"
+import Component, { tracked } from "sparkles-component"
 
 export default class Episode extends Component {
+  @tracked instructionsAreVisible = false
+
+  toggleInstructions() {
+    this.instructionsAreVisible = !this.instructionsAreVisible
+  }
 }
