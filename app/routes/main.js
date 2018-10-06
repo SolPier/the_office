6ac020e3a8e2bc900ff3e025813aa74a7b83998d
Aurelien 'Sol' Pieropan <sol.pieropan@gmail.com>
2018-10-06 17:23:12 +0200

    s3 data ok

--- app/routes/main.js
+++ app/routes/main.js
@@ -17,15 +17,15 @@ export default Route.extend({
       {
         number: 2,
         episodes: [
-          { number: 1, name: "The Dundies",                     videoUrl: 'https://openload.co/embed/qAdOk03kJNI/S02E01.mkv.mp4' },
-          { number: 2, name: "Sexual Harassment",               videoUrl: 'https://openload.co/embed/ACbw_0mObzo/S02E02.mkv.mp4' },
-          { number: 3, name: "Office Olympics",                 videoUrl: 'https://openload.co/embed/wA_jgjGOejA/S02E03.mkv.mp4' },
-          { number: 4, name: "The Fire",                        videoUrl: 'https://streamango.com/embed/oqbkkoeedmtfcssq'        },
-          { number: 5, name: "Halloween",                       videoUrl: 'https://streamango.com/embed/rdaactqdanksecsm'        },
-          { number: 6, name: "The Fight",                       videoUrl: 'https://streamango.com/embed/abnmlrspsoasffst'        },
-          { number: 7, name: "The Client",                      videoUrl: 'https://streamango.com/embed/abpqarkokbfdcpco'        },
-          { number: 8, name: "Performance Review",              videoUrl: 'https://openload.co/embed/aj6PNiYSnJw/S02E08.mkv.mp4' },
-          { number: 9, name: "E-mail Surveillance",             videoUrl: 'https://streamango.com/embed/bbkqtsfqprksldef'        },
+          { number: 1,  name: "The Dundies",                    videoUrl: 'https://openload.co/embed/qAdOk03kJNI/S02E01.mkv.mp4' },
+          { number: 2,  name: "Sexual Harassment",              videoUrl: 'https://openload.co/embed/ACbw_0mObzo/S02E02.mkv.mp4' },
+          { number: 3,  name: "Office Olympics",                videoUrl: 'https://openload.co/embed/wA_jgjGOejA/S02E03.mkv.mp4' },
+          { number: 4,  name: "The Fire",                       videoUrl: 'https://streamango.com/embed/oqbkkoeedmtfcssq'        },
+          { number: 5,  name: "Halloween",                      videoUrl: 'https://streamango.com/embed/rdaactqdanksecsm'        },
+          { number: 6,  name: "The Fight",                      videoUrl: 'https://streamango.com/embed/abnmlrspsoasffst'        },
+          { number: 7,  name: "The Client",                     videoUrl: 'https://streamango.com/embed/abpqarkokbfdcpco'        },
+          { number: 8,  name: "Performance Review",             videoUrl: 'https://openload.co/embed/aj6PNiYSnJw/S02E08.mkv.mp4' },
+          { number: 9,  name: "E-mail Surveillance",            videoUrl: 'https://streamango.com/embed/bbkqtsfqprksldef'        },
           { number: 10, name: "Christmas Party",                videoUrl: 'https://streamango.com/embed/netmmfbrkbqlmldt'        },
           { number: 11, name: "Booze Cruise",                   videoUrl: 'https://openload.co/embed/IhxUZ15-xN0/S02E11.mkv.mp4' },
           { number: 12, name: "The Injury",                     videoUrl: 'https://streamango.com/embed/lapnqmcdfptqnlka'        },
@@ -41,14 +41,40 @@ export default Route.extend({
           { number: 22, name: "Casino Night",                   videoUrl: 'https://openload.co/embed/bVwmPYDFadc/S02E22.mkv.mp4' }
         ]
       },
+      {
+        number: 3,
+        episodes: [
+          { number: 1,       name: "Gay Witch Hunt",        videoUrl: 'https://streamango.com/embed/fdftetalpdkpbmso'        },
+          { number: 2,       name: "The Convention",        videoUrl: 'https://streamango.com/embed/aldntelpksbctklb'        },
+          { number: 3,       name: "The Coup",              videoUrl: 'https://streamango.com/embed/ftmssspetkmbsmsf'        },
+          { number: 4,       name: "Grief Counseling",      videoUrl: 'https://streamango.com/embed/mertopscbcasapob'        },
+          { number: 5,       name: "Initiation",            videoUrl: 'https://streamango.com/embed/matcksfmtollpqne'        },
+          { number: 6,       name: "Diwali",                videoUrl: 'https://streamango.com/embed/aermqnfmotcqsala'        },
+          { number: 7,       name: "Branch Closing",        videoUrl: 'https://streamango.com/embed/aqnrppqpranobatt'        },
+          { number: 8,       name: "The Merger",            videoUrl: 'https://streamango.com/embed/kfmpktaafetlpdqn'        },
+          { number: 9,       name: "The Convict",           videoUrl: 'https://streamango.com/embed/dopasbkqrkmncrfc'        },
+          { number: '10-11', name: "A Benihana Christmas",  videoUrl: 'https://streamango.com/embed/bdalttanepalfnpk'        },
+          { number: 12,      name: "Back from Vacation",    videoUrl: 'https://streamango.com/embed/lrpsebnlbeetmnsm'        },
+          { number: 13,      name: "Traveling Salesmen",    videoUrl: 'https://openload.co/embed/a50dTka0dPw/S03E13.mkv.mp4' },
+          { number: 14,      name: "The Return",            videoUrl: 'https://openload.co/embed/NTc6uJxM-3U/S03E14.mkv.mp4' },
+          { number: 15,      name: "Ben Franklin",          videoUrl: 'https://streamango.com/embed/cflknbtdfrsnlctn'        },
+          { number: 16,      name: "Phyllis' Wedding",      videoUrl: 'https://streamango.com/embed/mfendrktoebakbnp'        },
+          { number: 17,      name: "Business School",       videoUrl: 'https://streamango.com/embed/nkmfnmqnecblqqdp'        },
+          { number: 18,      name: "Cocktails",             videoUrl: 'https://streamango.com/embed/bosfcqlkmomfbson'        },
+          { number: 19,      name: "The Negotiation",       videoUrl: 'https://streamango.com/embed/ncdmkeknkdrmsoen'        },
+          { number: 20,      name: "Safety Training",       videoUrl: 'https://streamango.com/embed/nlecqpacnpmerorc'        },
+          { number: 21,      name: "Product Recall",        videoUrl: 'https://openload.co/embed/cfd1WITbv6g/S03E21.mkv.mp4' },
+          { number: 22,      name: "Women's Appreciation",  videoUrl: 'https://openload.co/embed/BkDcke_35xg/S03E22.mkv.mp4' },
+          { number: 23,      name: "Beach Games",           videoUrl: 'https://openload.co/embed/ajK4F33luGI/S03E23.mkv.mp4' },
+          { number: "24-25", name: "The Job",               videoUrl: 'https://openload.co/embed/Hin2n2oP4D0/S03E24-25.mkv.mp4' }
+        ]
+      },
       // {
       //   number: 3,
       //   episodes: [
-      //     { number: 1, name: 'Gay Witch Hunt ', videoUrl: 'https://openload.co/embed/BFvO5w7oSG8/S03E01.mkv.mp4' },
-      //     { number: 2, name: 'The Convention ', videoUrl: 'https://openload.co/embed/KDRq_8Pcsl4/S03E02.mkv.mp4' },
-      //     { number: , name: '', videoUrl: '' },
+      //     { number: , name: "", videoUrl: '' },
       //   ]
-      // }
+      // },
     ]
   }
 });
