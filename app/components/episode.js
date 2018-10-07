fafd50f3ef29ce91f1a9726a18d3fa9644b4a2a5
Aurelien 'Sol' Pieropan <sol.pieropan@gmail.com>
2018-10-07 23:23:22 +0200

    removed unused 'later' import

--- app/components/episode.js
+++ app/components/episode.js
@@ -1,5 +1,4 @@
 import Component, { tracked } from "sparkles-component"
-import { later } from '@ember/runloop'
 
 export default class Episode extends Component {
   @tracked episodeIsVisible = false
