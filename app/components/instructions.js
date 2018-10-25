243daff6b8e92bdc4d904dcf65b802a27a238aa0
Aurelien 'Sol' Pieropan <aurelien.pieropan@doctolib.com>
2018-10-25 16:19:49 +0200

    WIP

--- app/components/instructions.js
+++
@@ -1,9 +0,0 @@
-import Component, { tracked } from "sparkles-component"
-
-export default class Episode extends Component {
-  @tracked instructionsAreVisible = false
-
-  toggleInstructions() {
-    this.instructionsAreVisible = !this.instructionsAreVisible
-  }
-}
---
+++ src/ui/components/TheOffice/component.ts
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import SeasonsData from './seasonsData.js';
+
+export default class TheOffice extends Component {
+  seasonsData = SeasonsData
+}
