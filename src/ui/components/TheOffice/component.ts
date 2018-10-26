6c15b9e586c847243b5c1193c90f1c5f804e3034
Aurelien 'Sol' Pieropan <aurelien.pieropan@doctolib.com>
2018-10-26 22:08:19 +0200

    starting redesign

--- src/ui/components/TheOffice/component.ts
+++ src/ui/components/TheOffice/component.ts
@@ -1,6 +1,23 @@
-import Component from '@glimmer/component';
-import SeasonsData from './seasonsData.js';
+import Component, { tracked } from '@glimmer/component'
+import Navigo from 'navigo'
+import SeasonsData from './seasonsData.js'
+
+const router = new Navigo(null, true)
 
 export default class TheOffice extends Component {
   seasonsData = SeasonsData
+
+  @tracked route: string = 'home'
+
+  constructor(options) {
+    super(options)
+    router
+      .on({
+        '/': () => { this.route = 'home'; },
+        '/episodes': () => { this.route = 'episodes'; },
+        '/about-show': () => { this.route = 'about-show'; },
+        '/about-site': () => { this.route = 'about-site'; },
+      })
+    .resolve();
+  }
 }
